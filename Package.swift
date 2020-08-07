16026e6c56d9f8d75d6f86e2e41780cbf3425c9f
Gonthier Renaud <yakusa120@hotmail.com>
2020-08-07 13:37:43 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,6 +17,7 @@ let package = Package(
     targets: [
         .target(
             name: "WKWebViewRTC",
+            path: "WKWebViewRTC",
             dependencies: ["GoogleWebRTC"]),
         .testTarget(
             name: "WKWebViewRTCTests",
