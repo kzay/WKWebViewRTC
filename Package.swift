6c53fc8b9e8dbe1aa0cd2b8f693d7bdc7115eebf
Gonthier Renaud <yakusa120@hotmail.com>
2020-08-07 13:27:00 -0400

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "WKWebViewRTC",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "WKWebViewRTC",
+            targets: ["WKWebViewRTC"]),
+    ],
+    dependencies: [
+        // no dependencies
+    ],
+    targets: [
+        .target(
+            name: "WKWebViewRTC",
+            dependencies: ["GoogleWebRTC"]),
+        .testTarget(
+            name: "WKWebViewRTCTests",
+            dependencies: ["WKWebViewRTC"]),
+    ]
+)
